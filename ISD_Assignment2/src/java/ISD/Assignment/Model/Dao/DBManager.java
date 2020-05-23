28830e08bbdcda79646e66b42def775e727a0dfb
eldermiang <61782036+eldermiang@users.noreply.github.com>
2020-05-23 15:03:05 +1000

    Adjust DBManager

--- ISD_Assignment2/src/java/ISD/Assignment/Model/Dao/DBManager.java
+++ ISD_Assignment2/src/java/ISD/Assignment/Model/Dao/DBManager.java
@@ -23,32 +23,36 @@ public class DBManager {
     }
     
     public User findUser(String email, String password) throws SQLException{
-        String read = "SELECT * FROM ISDUSER.Users WHERE EMAIL='" + email + "' " + " AND PASSWORD=" + password;
+        String read = "SELECT * FROM IOTUSER.Users WHERE EMAIL='" + email + "' " + " AND PASSWORD=" + password;
         ResultSet rs = st.executeQuery(read);
         
         while(rs.next()){
-            String userEmail = rs.getString(1);
+            String userID = rs.getString(1);
+            String userEmail = rs.getString(2);
             String userPass = rs.getString(3);
             if(userEmail.equals(email) && userPass.equals(password)){
-                String userName = rs.getString(2);
-                String userDOB = rs.getString(4);
-                String userFavCol = rs.getString(5);
-                return new User (userEmail, userName, userPass, userDOB, userFavCol);
+                String userName = rs.getString(4);
+                String userDOB = rs.getString(5);
+                String userGender = rs.getString(6);
+                String userAddress = rs.getString(7);
+                String userPostCode = rs.getString(8);
+                String userPhoneNumber = rs.getString(9);
+                return new User (userID, userEmail, userPass, userName, userDOB, userGender, userAddress, userPostCode, userPhoneNumber);
             }
         }
         return null;
     }
     
-    public void addUser(String email, String name, String password, String dob, String favcol) throws SQLException{
-        st.executeUpdate("INSERT INTO ISDUSER.Users " + "VALUES ('" + email + "', '" + name + "', " + password + "', " + dob + "', " + favcol + "')");
+    public void addUser(String id, String email, String password, String name, String dob, String gender, String address, String postCode, String phoneNumber) throws SQLException{
+        st.executeUpdate("INSERT INTO IOTUSER.Users " + "VALUES ('" + id + "', '" + email + "', " + password + "', " + name + "', " + dob + "', " + gender + "', " + address + "', " + postCode + "', " + phoneNumber + "')");
     }
     
-    public void updateUser(String email, String name, String password, String dob, String favcol) throws SQLException{
-        st.executeUpdate("UPDATE ISDUSER.Users SET NAME='" + name + "', PASSWORD='" + password + "', DOB='" + dob + "', FAVCOL='" + favcol + "' WHERE EMAIL='" + email + "'");
+    public void updateUser(String id, String email, String password, String name, String dob, String gender, String address, String postCode, String phoneNumber) throws SQLException{
+        st.executeUpdate("UPDATE IOTUSER.Users SET FULLNAME='" + name + "', PASSWORD='" + password + "', DOB='" + dob + "', GENDER='" + gender + "', ADDRESS='" + address + "', POSTCODE='" + postCode + "', PHONENUMBER='" + phoneNumber + "' WHERE EMAIL='" + email + "'");
     }
     
     public void deleteUser(String email) throws SQLException {
-        st.executeUpdate("DELETE FROM ISDUSER.Users WHERE EMAIL='" + email + "'");
+        st.executeUpdate("DELETE FROM IOTUSER.Users WHERE EMAIL='" + email + "'");
     }
     
     public ArrayList<User> fetchUsers() throws SQLException {
@@ -57,12 +61,16 @@ public class DBManager {
         ArrayList<User> temp = new ArrayList();
         
         while (rs.next()){
-            String email = rs.getString(1);
-            String name = rs.getString(2);
-            String password = rs.getString(3);
-            String dob = rs.getString(4);
-            String favcol = rs.getString(5);
-            temp.add(new User(email, name, password, dob, favcol));
+            String userID = rs.getString(1);
+            String userEmail = rs.getString(2);
+            String userPass = rs.getString(3);
+            String userName = rs.getString(4);
+            String userDOB = rs.getString(5);
+            String userGender = rs.getString(6);
+            String userAddress = rs.getString(7);
+            String userPostCode = rs.getString(8);
+            String userPhoneNumber = rs.getString(9);
+            temp.add(new User(userID, userEmail, userPass, userName, userDOB, userGender, userAddress, userPostCode, userPhoneNumber));
         }
         return temp;
     }
