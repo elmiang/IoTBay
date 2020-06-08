f8b4974158755502b25955b5702e512a74bfabe8
minoka5105 <61936313+minoka5105@users.noreply.github.com>
2020-06-08 10:52:49 +1000

    SQL Example UR Data Entries

--- ISD_Assignment2/build/web/db/insertSampleUserRecords.sql
+++ ISD_Assignment2/build/web/db/insertSampleUserRecords.sql
@@ -7,4 +7,24 @@
  * Author:  jacks
  * Created: Jun 8, 2020
  */
+INSERT INTO IOTUSER.USERRECORD (
 
+email,
+password,
+name,
+dob,
+gender,
+address,
+postcode,
+phoneNumber,
+role
+)
+VALUES
+('ed.reed@email.com','edreed555','Ed Reed','1994-10-10','M','17 Paris Street','2714','0843546','Customer'),
+('mike.freed@gmail.com','mf12345','Mike Freed','1990-12-19','M','79 Dolan Road','4374','01335436','Customer'),
+('ed.reef123@gmail.com','eddy123','Edward Reef','1995-09-10','M','10 Helen Avenue','6314','985735436','Customer'),
+('ge.hunter@email.com','hunter12345','George Hunter','1996-10-10','M','43 Random Road','1314','055735436','Staff'),
+('may123@gmail.com','123456','May Castillo','1997-11-20','F','61 Random Avenue','4314','7354356','Staff'),
+('adamlee@email.com','adlee312','Adam Lee','2000-02-10','M','34 Priam Street','2814','4360912','Staff'),
+('klay123@email.com','klay0987','Klay Roach','1985-07-24','M','67 Nestor Street','3319','0575436','Admin'),
+('jason@email.com','jbob982','Jason Bob','1997-10-14','M','98 Random Street','2314','75735436','Customer');
